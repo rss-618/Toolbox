301f34e04a430b71227cd087660d0c64dde3ef00
Ryan Schildknecht <schildknecht618@gmail.com>
2024-12-16 20:57:09 -0600

    Put some packages in here maybe make it work idk

--- Package.swift
+++ Package.swift
@@ -5,17 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "Toolbox",
+    
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Toolbox",
             targets: ["Toolbox"]),
+        
+    ],
+    dependencies: [
+        .package(url: "https://github.com/rss-618/AdaptiveSheet", exact: "1.0.0"),
+        .package(url: "https://github.com/rss-618/SwipeToDelete", exact: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Toolbox"),
+            name: "Toolbox", dependencies: ["AdaptiveSheet", "SwipeToDelete"]),
 
     ]
 )
